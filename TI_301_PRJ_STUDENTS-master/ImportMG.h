ef0d70270e87360dcd58d773f71855a2ecafe3b0
Userkol400 <kolia.bouthier@efrei.net>
2025-11-06 17:44:54 +0100

    Update ImportMG.h

--- TI_301_PRJ_STUDENTS-master/ImportMG.h
+++ TI_301_PRJ_STUDENTS-master/ImportMG.h
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "functions.c"
+
+t_cell* create_cell( int ver, int prob);
+
+t_list* create_empty_list();
+
+t_list* add_cell(t_cell* cell,t_list* list);
 
 typedef struct adjacency_list
 {
