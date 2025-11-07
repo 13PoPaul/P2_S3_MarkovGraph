5c1bb16e9b7a7f811cfffb9c78dcc4ddeb8d9ec8
13 PoPaul <paul.iglesias--tallon@efrei.net>
2025-11-07 23:37:30 +0100

    Part 1 Done

--- TI_301_PRJ_STUDENTS-master/main.c
+++ TI_301_PRJ_STUDENTS-master/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "ImportMG.h"
 #include "check.c"
+#include "mermaid.h"
 
 int main() {
     printf("Hello, World!\n");
-    adjacency_list * M_Graph = readGraph("data/exemple1.txt");
+    adjacency_list * M_Graph = readGraph("data/exemple_valid_step3.txt");
     display_adjency_list(M_Graph);
     isMarkovGraph(M_Graph);
+    exportToMermaid(M_Graph, "graph_mermaid.txt");
     return 0;
 }
